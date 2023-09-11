33c04be4bd7c160c13ad32b749edafafae83d837
Ryan-Nam <alfofh7060@gmail.com>
2023-09-11 18:52:47 +1000

    Set Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
-import './App.css';
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <></>
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   );
 }
 

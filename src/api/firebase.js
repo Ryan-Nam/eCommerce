52013979813d51477ea03d62bf90c3b3d0f70849
Ryan-Nam <alfofh7060@gmail.com>
2023-09-21 22:14:15 +1000

    Add Logout and onAuthStateChange function

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged  } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,12 +18,30 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
 
-
-export function login() {
-    signInWithPopup(auth, provider)
+// login을했다면, 유효한 user의 객체를 return
+export async function login() {
+    return signInWithPopup(auth, provider)
   .then((result) => {
     const user = result.user;
     console.log(user);
+    return user;
 
   }).catch(console.error);
+}
+
+// signout을 했다면, 결과에 관계없이 null return
+export async function logout() {
+    return signOut(auth).then(()=> null)
+}
+
+//콜백함수를 만들어놓고, 유저라는 상태가 변경되는 이벤트가 발생할때마다
+// 콜백함수 호출
+export function onUserStateChange(callback) {
+onAuthStateChanged(auth, (user) => {
+    callback(user);
+    // if (user) { 
+    //   const uid = user.uid;
+    // } else {
+    // }
+  });
 }

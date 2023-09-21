d489ef1309d5e0000a038fea5c3f3f2788e1d8c3
Ryan-Nam <alfofh7060@gmail.com>
2023-09-21 19:02:42 +1000

    Add header styling

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App";
+import Home from "./pages/Home";
+import AllProducts from "./pages/AllProducts";
+import MyCart from "./pages/MyCart";
+import NewProduct from "./pages/NewProduct";
+import NotFound from "./pages/NotFound";
+import ProductDetail from "./pages/ProductDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products",
+        element: <AllProducts />,
+      },
+      {
+        path: "/products/new",
+        element: <NewProduct />,
+      },
+      {
+        path: "/products/:id",
+        element: <ProductDetail />,
+      },
+      {
+        path: "/carts",
+        element: <MyCart />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

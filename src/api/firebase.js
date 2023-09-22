fd0500c064c5cb556104e8146f2a842547977212
Ryan-Nam <alfofh7060@gmail.com>
2023-09-22 17:23:04 +1000

    Add Admin user with API - realDatabase

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,6 +6,9 @@ import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChang
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import { getDatabase, ref, child, get } from "firebase/database";
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -17,31 +20,47 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+const database = getDatabase(app);
 
 // login을했다면, 유효한 user의 객체를 return
-export async function login() {
-    return signInWithPopup(auth, provider)
-  .then((result) => {
-    const user = result.user;
-    console.log(user);
-    return user;
-
-  }).catch(console.error);
+export function login() {
+     signInWithPopup(auth, provider).catch(console.error);
 }
 
 // signout을 했다면, 결과에 관계없이 null return
-export async function logout() {
-    return signOut(auth).then(()=> null)
+export function logout() {
+    signOut(auth).catch(console.error);
 }
 
 //콜백함수를 만들어놓고, 유저라는 상태가 변경되는 이벤트가 발생할때마다
 // 콜백함수 호출
 export function onUserStateChange(callback) {
-onAuthStateChanged(auth, (user) => {
-    callback(user);
+onAuthStateChanged(auth, async (user) => {
+    // user && adminUser(user);
+    // 1.사용자가 있는 경우에 (로그인한 경우)
+    const updatedUser = user ? await adminUser(user) : null;
+
+    // console.log(user);
+    callback(updatedUser);
     // if (user) { 
     //   const uid = user.uid;
     // } else {
     // }
   });
+}
+
+async function adminUser(user) {
+        // 2. 사용자가 어드민 권한을 가지고 있는지 확인!
+    // 3. {user, isAdmin: true/false}
+    return get(ref(database, 'admins')).then((snapshot) => {
+        if(snapshot.exists()) {
+            const admins = snapshot.val();
+            // console.log(admins);
+            const isAdmin = admins.includes(user.uid);
+            // console.log(isAdmin);
+            return {...user, isAdmin}
+        } 
+        return user;
+    })
+
 }

7131ae2585810ecfcccbb3b810fb88274a330e48
Ryan-Nam <alfofh7060@gmail.com>
2023-09-22 22:03:47 +1000

    Add admin and directoy protection, redirection

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,20 @@ import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
 import { login, logout, onUserStateChange } from '../api/firebase';
 import User from './User';
+import Button from './ui/Button';
+import { useAuthContext } from './context/AuthContext';
 
 export default function Navbar() {
-  const [user, setUser] = useState();
+  // const [user, setUser] = useState();
 
-  useEffect(()=> {
-    onUserStateChange((user) =>{
-      console.log(user);
-      setUser(user);
-    })
-  }, [])
+  // useEffect(()=> {
+  //   onUserStateChange((user) =>{
+  //     console.log(user);
+  //     setUser(user);
+  //   })
+  // }, [])
+
+  const {user, login, logout} = useAuthContext();
 
 
   return (
@@ -24,13 +28,15 @@ export default function Navbar() {
       </Link>
       <nav className='flex items-center gap-4 font-semibold'>
         <Link to="/products">Products</Link>
-        <Link to="/carts">Products</Link>
-        <Link to="/products/new" className='text-2xl'>
+        {user && <Link to="/carts">Carts</Link>}
+        {user && user.isAdmin && <Link to="/products/new" className='text-2xl'>
           <BsFillPencilFill />
         </Link>
+        }
+        
         {user && <User user={user} />}
-        {!user && <button onClick={login}>Login</button>}
-        {user && <button onClick={logout}>Logout</button>}
+        {!user && <Button text={'Login'} onClick={login}></Button>}
+        {user && <Button text={'Logout'} onClick={logout}></Button>}
       </nav>
     </header>
   );
--- src/index.js
+++ src/index.js
@@ -1,49 +1,58 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from "./App";
-import Home from "./pages/Home";
-import AllProducts from "./pages/AllProducts";
-import MyCart from "./pages/MyCart";
-import NewProduct from "./pages/NewProduct";
-import NotFound from "./pages/NotFound";
-import ProductDetail from "./pages/ProductDetail";
+import App from './App';
+import Home from './pages/Home';
+import AllProducts from './pages/AllProducts';
+import MyCart from './pages/MyCart';
+import NewProduct from './pages/NewProduct';
+import NotFound from './pages/NotFound';
+import ProductDetail from './pages/ProductDetail';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     errorElement: <NotFound />,
     children: [
       {
         index: true,
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/products",
+        path: '/products',
         element: <AllProducts />,
       },
       {
-        path: "/products/new",
-        element: <NewProduct />,
+        path: '/products/new',
+        element: (
+          <ProtectedRoute requredAdmin>
+            <NewProduct />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: "/products/:id",
+        path: '/products/:id',
         element: <ProductDetail />,
       },
       {
-        path: "/carts",
-        element: <MyCart />,
+        path: '/carts',
+        element: (
+          <ProtectedRoute>
+            <MyCart />
+          </ProtectedRoute>
+        ),
       },
     ],
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
